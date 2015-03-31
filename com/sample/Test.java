72aa0d137a778472a5bd49f3ac012a2f14184410
bobanpaul <bobanpaulp@gmail.com>
2015-03-31 11:14:42 -0700

    test

---
+++ com/sample/Test.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sample Run");
+
+	}
+
+}
